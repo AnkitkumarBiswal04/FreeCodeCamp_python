4f731527509a77c6c223177074482b9cef008ff8
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-01 11:45:26 +0530

    input from users

---
+++ p6_lists.py
@@ -0,0 +1,14 @@
+#in list we keep structured data
+#let make a list of friends
+
+friends=["Ankit","Arun","Ashok","Anand","Aroma"]
+friends[0]="saraswati" #with this we can update the value
+print(friends) #prints all the element in a list
+print(friends[0]) #prints the first element
+print(friends[-1]) #prints the last element
+print(friends[1:]) #prints all  the element after 1 index i.e Arun and Ashok
+print(friends[1:3]) #prints the range of the element in between 1 and 3
+
+
+
+
