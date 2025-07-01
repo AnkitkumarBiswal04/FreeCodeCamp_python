827a50efa279c294a1b14ea2fb001267f75e3627
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-01 14:38:35 +0530

    functions

--- p9_functions.py
+++ p9_functions.py
@@ -1,10 +1,11 @@
 # we are going to create a function that is going to say hi to the users
 
 #defining the function
-def say_hi():
-    print("hello users")
+def say_hi(number,name,age): #inside the brackets are the parameters
+    print("hello user "+number+" : "+name+" is "+age+" years old")
 
 #calling the functionS
 print("Top")
-say_hi()
+say_hi("1","Ankit","24")
+say_hi("2","Arun","56")
 print("bottom")
