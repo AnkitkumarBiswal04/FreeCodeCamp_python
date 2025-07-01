a46b9cac08ecd49a5355d78393608cf42f832242
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-01 12:09:46 +0530

    list function updated

--- p7_listfunctions.py
+++ p7_listfunctions.py
@@ -1,6 +1,6 @@
 lucky_number=[1,2,3,4,5,6]
 friends=["Ankit","Arun","Ashok","Anand","Aroma","saraswati"]
-friends.append("Babita") #add element to the list
+friends.append("Babita") #add a element to the list
 print(friends)
 friends.extend(lucky_number) # merges two lists into one list
 print(friends)
