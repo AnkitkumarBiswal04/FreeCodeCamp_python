c8cb6e2f0fe08c6bfb4ebe8221061492fe957ccc
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-01 13:04:20 +0530

    tuples

--- p8_tuples.py
+++ p8_tuples.py
@@ -1,3 +1,9 @@
-# tuples are like list that stores the data and it is immutable i.e cannot be modified
+# tuples are like list that stores the data, and it is immutable that is cannot be modified
+# #unlike list which can be modified
 coordinates=(4,5)
+coordinates1=[(4,5),(5,6),(6,7)]
+print(coordinates[0])
 print(coordinates[1])
+print(coordinates1[1])
+#practical use of tuples where data do not change
+#A majority of time we use list
