6f122e5bb8634666412170920c5f7cb2de98a5c0
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-01 12:16:59 +0530

    insert function in list

--- p7_listfunctions.py
+++ p7_listfunctions.py
@@ -1,7 +1,9 @@
 lucky_number=[1,2,3,4,5,6]
 friends=["Ankit","Arun","Ashok","Anand","Aroma","saraswati"]
-friends.append("Babita") #add a element to the list
+friends.append("Babita") #add an element to the list
 print(friends)
 friends.extend(lucky_number) # merges two lists into one list
+print(friends)
+friends.insert(1,"kelly") # inserts an element in between the list
 print(friends)
 
