ac69a298224c029330f2b50d433c67a403a9e12d
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-02 13:15:52 +0530

    functions

--- p9_functions.py
+++ p9_functions.py
@@ -2,10 +2,10 @@
 
 #defining the function
 def say_hi(number,name,age): #inside the brackets are the parameters
-    print("hello user "+number+" : "+name+" is "+age+" years old")
+    print("hello user "+number+" : "+name+" is "+str(age)+" years old")
 
 #calling the functionS
 print("Top")
-say_hi("1","Ankit","24")
-say_hi("2","Arun","56")
+say_hi("1","Ankit",24) # note we can pass integer but we need to make integer as string
+say_hi("2","Arun",56)
 print("bottom")
