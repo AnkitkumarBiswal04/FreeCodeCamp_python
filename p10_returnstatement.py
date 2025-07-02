0bdc6dafe09547a7685390a34d50491f61e560ff
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-02 13:26:25 +0530

    returnstatement

---
+++ p10_returnstatement.py
@@ -0,0 +1,10 @@
+# when we write the function sometimes we want the information back there we use return keyword
+#function to cube a number
+
+from math import *
+def cube(num):
+    return num*num*num #this return statement returns the value
+# note after return statement we cannot put any code
+
+print(cube(3)) # 3 is the parameter
+print(cube(4))
