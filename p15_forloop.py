dd5717ae9af1315fc1b45c1ba395ca8c9fee1b3e
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-02 16:45:39 +0530

    forloop

---
+++ p15_forloop.py
@@ -0,0 +1,20 @@
+# for loop is used for accessing the elements in an array or string or series of numbers
+
+#for loop in case of series of number
+# it prints number from 0 to 9
+for index in range(10):
+    print(index)
+
+#for loop in case of series of number
+# it prints number from 4 to 9
+for index in range(3,10):
+    print(index)
+
+#for loop in case of array
+friends=["ankit","Arun","Ashok"]
+for friend in friends:
+    print(friend)
+
+#for loop in case of string
+for letter in "Ankit Kumar Biswal":
+    print(letter)
