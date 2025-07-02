4ee1651770821a0dafa7b7780c6020fc04921f88
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-02 19:03:20 +0530

    buildingatranslator

---
+++ p18-buildingatranslator.py
@@ -0,0 +1,13 @@
+# check whether it is a vowel or not
+def translate(phrase):
+    translation=""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation=translation+"G"
+            else:
+                translation = translation + "g"
+        else:
+            translation=translation+letter
+    return translation
+print(translate(input("Enter the text :")))
