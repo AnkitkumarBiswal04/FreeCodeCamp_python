d91b62f7362fa5770c1c0c75e6224d12e5132d10
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2025-07-02 16:35:27 +0530

    whileloop

---
+++ p14_whileloop.py
@@ -0,0 +1,6 @@
+#while loops is going to run the loop multiple times until certain condition is false
+i=int(input("enter the value of i :"))
+while i <= 10:
+    print(i)
+    i += 1
+print("Done with loop")
